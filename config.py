c40c6e401b9c90d3b1f31c9e3d292b2e381c546a
ConfuSomu <emilemeekel+github@gmail.com>
2020-06-04 11:57:45 -0400

    Move configuration into config.py
    Moving the configuration into a new file makes it modular. Changes to
    the way the configuration is read can be done without having to modify
    any other source files.

---
+++ config.py
@@ -0,0 +1,37 @@
+def general():
+    conf = {}
+    conf["ROOT_DIR"] = 'images' # Directory containing subdirectories with images
+    #conf["FILE_FMT"] = '{root}/{subdir} - {frame}.png' # Format string for output image
+    conf["FILE_FMT"] = '/tmp/{root}/{subdir} - {frame}.png' # Format string for output image
+    conf["TEXT_OVERLAY"] = ['Love', 'Like', 'Dislike', 'Haha', '!!', '?'] # Overlays to apply on the images, in this case: iMessage reactions
+    return conf
+
+def canvas():
+    conf = {}
+    conf["FONT"] = {}
+
+    conf["MODE"]     = 'RGB'
+    conf["COLOR"]    = 0
+    conf["COLS"]     = 3    # Number of colums
+    conf["WIDTH"]    = 500  # Total width
+    conf["C_HEIGHT"] = 200  # Cell height
+
+    conf["FONT"]["FILE"] = 'Jost-Regular.ttf' # TrueType/OpenType font
+    conf["FONT"]["SIZE"] = 13 # Size in points, this should be set in proportion to the cell size
+    conf["FONT"]["COLOR"] = 255 # Fill color of the font
+    conf["FONT"]["S_COLOR"] = 0 # Color of the text stroke
+    conf["FONT"]["S_WIDTH"] = 1 # Width of the text stroke
+
+    conf["ROWS"] = None
+    conf["HEIGHT"] = None
+    conf["C_WIDTH"] = conf["WIDTH"]/conf["COLS"]
+    return conf
+
+def ffmpeg():
+    conf = {}
+    conf["FFMPEG"] = 'ffmpeg' # Location of the ffmpeg binary
+    conf["CODEC"] = 'ffv1' # Codec to use for the generated video, prefer a lossless one for better fidelity
+    #conf["FILENAME"] = '{root}/{subdir}.avi' # Filename format string
+    conf["FILENAME"] = '/tmp/{root}/{subdir}.avi' # Filename format string
+    conf["FPS"] = '30' # FPS of the resulting video file
+    return conf
--- main.py
+++ main.py
@@ -4,44 +4,17 @@ from PIL import Image
 import math
 from os import walk, listdir
 from os.path import isfile, join
+import config
 from image import is_animated, rotate, combineImages
 from video import animate, getFps, avgFps
 
-ROOT_DIR = 'images' # Directory containing subdirectories with images
-#FILE_FMT = '{root}/{subdir} - {frame}.png' # Format string for output image
-FILE_FMT = '/tmp/{root}/{subdir} - {frame}.png' # Format string for output image
-TEXT_OVERLAY = ['Love', 'Like', 'Dislike', 'Haha', '!!', '?'] # Overlays to apply on the images, in this case: iMessage reactions
-
-# Canvas parameters
-CANVAS_ATT = {}
-CANVAS_ATT["FONT"] = {}
-
-CANVAS_ATT["MODE"]     = 'RGB'
-CANVAS_ATT["COLOR"]    = 0
-CANVAS_ATT["COLS"]     = 3    # Number of colums
-CANVAS_ATT["WIDTH"]    = 500  # Total width
-CANVAS_ATT["C_HEIGHT"] = 200  # Cell height
-
-CANVAS_ATT["FONT"]["FILE"] = 'Jost-Regular.ttf' # TrueType/OpenType font
-CANVAS_ATT["FONT"]["SIZE"] = 13 # Size in points, this should be set in proportion to the cell size
-CANVAS_ATT["FONT"]["COLOR"] = 255 # Fill color of the font
-CANVAS_ATT["FONT"]["S_COLOR"] = 0 # Color of the text stroke
-CANVAS_ATT["FONT"]["S_WIDTH"] = 1 # Width of the text stroke
-
-CANVAS_ATT["ROWS"] = None
-CANVAS_ATT["HEIGHT"] = None
-CANVAS_ATT["C_WIDTH"] = CANVAS_ATT["WIDTH"]/CANVAS_ATT["COLS"]
-
-# FFmpeg parameters
-FFMPEG = {}
-FFMPEG["FFMPEG"] = 'ffmpeg' # Location of the ffmpeg binary
-FFMPEG["CODEC"] = 'ffv1' # Codec to use for the generated video, prefer a lossless one for better fidelity
-#FFMPEG["FILENAME"] = '{root}/{subdir}.avi' # Filename format string
-FFMPEG["FILENAME"] = '/tmp/{root}/{subdir}.avi' # Filename format string
-FFMPEG["FPS"] = '30' # FPS of the resulting video file
+# Load configuration
+CONFIG = config.general()
+CANVAS_ATT = config.canvas()
+FFMPEG = config.ffmpeg()
 
 # Walk in subdirectories (jour 1, jour 2, ...)
-for root, dirs, _ in walk(ROOT_DIR):
+for root, dirs, _ in walk(CONFIG["ROOT_DIR"]):
     print("dirs in root: "+str(dirs)) # Debug
     
     # For each subdir
@@ -75,10 +48,10 @@ for root, dirs, _ in walk(ROOT_DIR):
         # Create a canvas image that will contain the other images
         canvas = Image.new(CANVAS_ATT["MODE"], (CANVAS_ATT["WIDTH"], CANVAS_ATT["HEIGHT"]), CANVAS_ATT["COLOR"])
         
-        frames = combineImages(canvas, taggedImages, CANVAS_ATT, TEXT_OVERLAY, [FILE_FMT, [root, subdir]])
+        frames = combineImages(canvas, taggedImages, CANVAS_ATT, CONFIG["TEXT_OVERLAY"], [CONFIG["FILE_FMT"], [root, subdir]])
         
         # If multiple frames have been created,
         # Than create a video file…
         if frames > 1:
             fps = avgFps(taggedImages)
-            animate(frames, fps, FFMPEG, CANVAS_ATT, [FILE_FMT, [root, subdir]])
+            animate(frames, fps, FFMPEG, CANVAS_ATT, [CONFIG["FILE_FMT"], [root, subdir]])
